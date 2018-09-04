6137144705bf581753da2927787e3e9c8311e2ce
trungtn97 <trc.trungdd2010@gmail.com>
2018-09-04 14:15:49 +0700

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Viet cai eo gi cung dc");
+            Console.ReadLine();
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
